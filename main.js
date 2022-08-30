62b9de7792e52c7c84192674c418ce9f3ceb1172
AmarantaC <amarcitla@gmail.com>
2022-08-30 10:11:29 -0500

    Ejercicio clase ciclos

---
+++ main.js
@@ -0,0 +1,39 @@
+
+//--------- Ejercicio 1 --------//
+
+/*for(let i = 0; i < 3; i++) {
+
+    let fichasUsuario = parseInt(prompt("¿Cuantas fichas tienes? "));
+    let fichasPromocion = (fichasUsuario + 30);
+
+    if(fichasPromocion >= 100) {
+        alert ("Tu total de fichas es: " + fichasPromocion + " ¡Puedes canjearlas!");
+        break; 
+    }else {
+        alert ("Tu total de fichas es: " + fichasPromocion + " ¡Intentalo otra vez!");        
+    }
+};*/
+
+//---------Ejercicio 2-----------//
+
+let nombre = prompt("Ingresa tu nombre: ");
+
+
+while(nombre != "ESC"){
+    alert("Hola " + nombre);
+    nombre = prompt("Ingresa tu nombre: ");
+}
+
+
+//-------- Ejercicio 3 ---------//
+
+/*let numero = parseInt(prompt("Escribe tu número "));
+
+for(let i = 0; i < (numero); i++){
+    console.log("hola");
+}*/
+
+
+
+
+
